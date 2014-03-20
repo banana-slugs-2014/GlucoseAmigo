536675816d1002a322f1507101bd00c0c55a69dd
Chris Woodrich <cwoodrich@gmail.com>
2014-03-20 15:05:56 -0700

    Rails skeleton, project folder, gemfile

---
+++ DiabeticBud/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiabeticBud::Application.config.secret_token = '8972c4aa0831b841c1c46222ffe7e1069e8c105248174d0f3e276a604b6039f61c1f7f806f013829adb98f446be04852bacce3a35d790f3b4d00498eedfbac21'
