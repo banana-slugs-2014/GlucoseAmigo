7ace40cebca35fb19cdf47d0112fe2d915506f9b
Chris Woodrich <cwoodrich@gmail.com>
2014-03-21 12:04:35 -0700

    Add to record controller methods

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -16,25 +16,27 @@ class RecordsController < ApplicationController
     @record = Record.create(params[:record])
     if @record.save
       current_user.records << @record
-      render partial: 'shared/record' locals {record: @record}
+      #render partial: 'shared/record' locals {record: @record}
     else
-
+      #render partial :edit
+    end
   end
 
   def edit
     @record = Record.find(params[:id])
-
-
-
-
   end
 
   def update
     @record = Record.find(params[:id])
-    @record.update_attributes(params)
+    redirect_to record_path(@record) and return if @record.update_attributes(params[:record])
+    redirect_to edit_record_path(@record) #sad path
+    #render partial: 'shared/record' locals {record: @record}
   end
 
   def delete
+    record = Record.find(params[:id])
+    record.destroy
+    redirect_to root_path
   end
 
 end
