fe3903123430408161b42f4c84583fca453e554a
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-20 18:51:15 -0700

    Add Diabetic model tests

---
+++ spec/models/diabetic.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+describe Diabetic do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :age }
+  it { should validate_presence_of :email }
+  it { should allow_value("test@test.com").for :email}
+  it { should_not allow_value("qlskr.com","teae",12).for :email}
+end
