282fb080f4f1fd32f4407392efb52e2a58a42cb3
Chris Woodrich <cwoodrich@gmail.com>
2014-03-21 11:45:58 -0700

    Add basic routes to RecordController

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,21 +1,37 @@
 class RecordsController < ApplicationController
 
   def index
+    @records = Record.all
   end
 
   def show
+    @record = Record.find(params[:id])
   end
 
   def new
+    @record = Record.new
   end
 
   def create
+    @record = Record.create(params[:record])
+    if @record.save
+      current_user.records << @record
+      render partial: 'shared/record' locals {record: @record}
+    else
+
   end
 
   def edit
+    @record = Record.find(params[:id])
+
+
+
+
   end
 
   def update
+    @record = Record.find(params[:id])
+    @record.update_attributes(params)
   end
 
   def delete
