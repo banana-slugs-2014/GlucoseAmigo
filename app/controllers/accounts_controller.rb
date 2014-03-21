6b0b94ccb164ddce5af221657e23ddde390ac107
Chermaine Zimmerman <chermainespence@Chermaines-MacBook-Pro.local>
2014-03-21 14:13:17 -0700

    adds create path for account

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,22 @@
+class AccountsController < ActionController::Base
+
+  def index
+    @account = Account.find(1)
+    render 'accounts/testing'
+  end
+
+  def new
+    render  :partial => 'shared/sign_up',
+    :locals => {
+      account: Account.new
+    }
+  end
+
+  def create
+    account = Account.create(params['account'])
+    puts account.errors.any?
+    redirect_to accounts_path
+  end
+
+
+end
