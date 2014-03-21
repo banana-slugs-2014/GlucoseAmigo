a0c6ae20993092b5a796eb59f13e958e7f45cca4
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-21 15:48:03 -0700

    Add a birth_date field instead of age

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -1,9 +1,15 @@
 class Diabetic < ActiveRecord::Base
-  attr_accessible :name, :age, :email
+  attr_accessible :name, :age, :email, :birth_date
   belongs_to :account
   belongs_to :doctor
   validates_presence_of :age, :name, :email
   validates_format_of :email, :with => /^\w+@\w+\.\w{2,5}$/
   validates_format_of :age, :with => /^\d+$/ # We validate the format and that it is not an integer at the same time
   has_many :records
+
+  def birth_date=(bd)
+  end
+
+  def birth_date
+  end
 end
