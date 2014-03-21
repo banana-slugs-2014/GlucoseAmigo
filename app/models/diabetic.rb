158628faeb3f400104036a4d94462f71f8a5de71
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-20 18:50:26 -0700

    Add new tests for format

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -1,4 +1,5 @@
 class Diabetic < ActiveRecord::Base
   validates_presence_of :age, :name, :email
   # Validation for Phone sets confirmed
+  validates_format_of :email, :with => /^\w+@\w+\.\w{2,5}/
 end
