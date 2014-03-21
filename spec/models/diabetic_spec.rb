9ba591cfb3e7aec282ee0475b0c6da27da74a781
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-21 16:56:21 -0700

    Change unit test to pass the new birth_date

--- spec/models/diabetic_spec.rb
+++ spec/models/diabetic_spec.rb
@@ -4,7 +4,13 @@ describe Diabetic do
   it { should validate_presence_of :email }
   it { should allow_value("test@test.com").for :email}
   it { should_not allow_value("qlskr.com","teae",12).for :email}
-  it { should allow_value(Date.today - 400).for :birth_date }
+  it { should allow_value(
+    {
+        year: Date.today.year - rand(15),
+        month: Date.today.month,
+        day: Date.today.day
+    }
+    ).for :birth_date }
   it { should belong_to :doctor }
   it { should belong_to :account }
 end
