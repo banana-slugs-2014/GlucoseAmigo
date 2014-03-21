7da043574bc81879325eacb3f835e93a96dace4a
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-20 18:58:01 -0700

    Add age format verification

--- spec/models/diabetic.rb
+++ spec/models/diabetic.rb
@@ -5,4 +5,6 @@ describe Diabetic do
   it { should validate_presence_of :email }
   it { should allow_value("test@test.com").for :email}
   it { should_not allow_value("qlskr.com","teae",12).for :email}
+  it { should allow_value("12").for :age }
+  it { should_not allow_value(12,"test").for :age }
 end
