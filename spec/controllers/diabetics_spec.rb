12250725f1a84f234ead8bc21b4a1c706fb76dd9
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-21 22:48:28 -0700

    Refactor specs for diabetics controller

--- spec/controllers/diabetics_spec.rb
+++ spec/controllers/diabetics_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe DiabeticsController do
   before(:each) do
     @account = Account.create(username: 'tester', email: 'test@test.com', password: 'password', password_confirmation: 'password')
+    @diabetic = Diabetic.create(name: 'Glen', email: 'test@test.com', birth_date: birth_date)
     request.session[:user_id] = @account.id
   end
 
@@ -30,11 +31,10 @@ describe DiabeticsController do
   }
 
   end
-  describe 'Diabetics creation' do
+  describe '#create' do
     context 'with valid params' do
       it 'should add a diabetic when adding on the correct path' do
         expect{
-          p @account
           post :create, valid_params
           expect(response).to be_redirect
         }.to change {Diabetic.count}.by 1
@@ -45,24 +45,25 @@ describe DiabeticsController do
       it 'should not add a diabetic when adding on the correct path' do
         expect{
           post :create, invalid_params
-
           expect(response).to be_redirect
         }.to_not change(Diabetic, :count)
       end
     end
   end
 
-  describe 'Diabetics Update' do
-    before(:each) do
-      @diabetic = Diabetic.create(name: 'Glen', email: 'test@test.com', birth_date: birth_date)
-    end
+
+describe '#edit' do
+  it 'should have an edit page' do
+    get :edit, id: @diabetic.id, account_id: @account.id
+    expect(response).to be_ok
+  end
+end
+
+describe '#update' do
 
     let(:valid_edit_params) do
       {
-      diabetic: {
-        name: "Captain Mando",
-        email: Faker::Internet.email
-      },
+      diabetic: attributes_for(:diabetic),
       birth_date: birth_date,
       id: @diabetic.id,
       account_id: @account.id
@@ -78,24 +79,22 @@ describe DiabeticsController do
       }
     end
 
-    it 'should have an edit page' do
-      get :edit, id: @diabetic.id, account_id: @account.id
-      expect(response).to be_ok
-    end
-
     context 'with valid parameters' do
       it 'should edit a Diabetic' do
         expect {
           put :update, valid_edit_params
-          p valid_edit_params
-          p @diabetic
-          p @account
           expect(response).to be_redirect
         }.to change { Diabetic.find(@diabetic.id).name }
       end
-
-
     end
 
+    context 'with invalid parameters' do
+      it 'should not edit a Diabetic' do
+        expect {
+          put :update, invalid_edit_params
+          expect(response).to be_redirect
+        }.to_not change { Diabetic.find(@diabetic.id).name }
+      end
+    end
   end
 end
