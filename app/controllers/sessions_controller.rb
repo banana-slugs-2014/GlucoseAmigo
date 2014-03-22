b08a1211ee48fb3e1f221f81ad8a3d0bd949aa2b
Apprentice <apprentice@dbc20.local>
2014-03-21 18:50:36 -0700

    Correct the routes to make the tests pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,16 @@ class SessionsController < ApplicationController
     account = Account.find_by_username(params[:username]).try(:authenticate, params[:password])
     if account
       session[:user_id] = account.id
-      redirect_to :back# diabetic dashboard
+      redirect_to new_session_path# diabetic dashboard
     else
       flash[:error] = 'Invalid Login information'
-      redirect_to :back
+      redirect_to new_session_path
     end
   end
 
   def destroy
     reset_session
     flash[:message] = 'You are logged out.'
-    redirect_to root_path# home page
+    redirect_to new_session_path# home page
   end
 end
