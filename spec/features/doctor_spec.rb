2fda4077d366caf22b17d3b7793586acdee751d5
Tim <tyau52@gmail.com>
2014-03-21 22:25:31 -0700

    Add feature spec, all green Edit model Edit views

--- spec/features/doctor_spec.rb
+++ spec/features/doctor_spec.rb
@@ -6,6 +6,8 @@ describe "doctors" do
 	let(:doc_attr) { attributes_for :doctor }
 	let!(:pre_created_doc_attr) { attributes_for :doctor }
 
+
+
   describe "User can see base page" do
     it "by visting the doctors page" do
       visit doctors_path
@@ -13,140 +15,73 @@ describe "doctors" do
     end
   end
 
-# #==================================================================
-# #======================create questions============================
-# #==================================================================
-
-#   describe "User can visit a question creation page" do
-#     it "by clicking on ask a question" do
-#       visit root_path
-#       click_on "Ask a question!"
-#       expect(page.status_code).to eq(200)
-#       expect(page).to have_content "Question"
-#       expect(page).to have_css("form")
-#     end
-
-#     context "with valid parameters" do
-#       it "should be able to create a new question" do
-#         expect{
-#           visit root_path
-#           click_on "Ask a question!"
-#           fill_in "question[title]", with: test_title
-#           fill_in "question[content]", with: test_content
-#           click_on "Create Question"
-#         }.to change{Question.count}.by(1)
-#         expect(page).to have_content test_title
-#       end
-#     end
-
-#     context "with invalid parameters" do
-#       it "title cannot be blank" do
-#         expect{
-#           visit root_path
-#           click_on "Ask a question!"
-#           fill_in "question[content]", with: test_content
-#           click_on "Create Question"
-#         }.to_not change{Question.count}.by(1)
-#       end
-
-#       it "content cannot be blank" do
-#         expect{
-#           visit root_path
-#           click_on "Ask a question!"
-#           fill_in "question[title]", with: test_title
-#           click_on "Create Question"
-#         }.to_not change{Question.count}.by(1)
-#       end
-#     end
-#   end
-
-# #==================================================================
-# #=======================delete question============================
-# #==================================================================
-
-#   describe "User can delete their own question" do
-#     it "by clicking on delete question" do
-#       visit root_path
-#       click_on "Ask a question!"
-#       fill_in "question[title]", with: test_title
-#       fill_in "question[content]", with: test_content
-#       click_on "Create Question"
-#       expect{
-#         click_on "Delete Question"
-#       }.to change{Question.count}.by(-1)
-#       expect(page).to_not have_content(test_title)
-#     end
-#   end
-
-#   describe "User cannot delete questions not by them or when logged out" do
-#     it "should not see any 'delete' button on the page when logged out" do
-#       visit root_path
-#       click_on "Log Out"
-#       expect(page).to_not have_content("Delete Question")
-#     end
-#   end
-
-# #====================================================================
-# #=========================edit question==============================
-# #====================================================================
-
-#   describe "User can edit an existing question" do
-
-#     before(:each) do
-#       visit root_path
-#       click_on "Ask a question!"
-#       fill_in "question[title]", with: test_title
-#       fill_in "question[content]", with: test_content
-#       click_on "Create Question"
-#     end
-
-#     it "by clicking on edit" do
-#       visit root_path
-#       click_on "Edit Question"
-#       expect(page.status_code).to eq(200)
-#       expect(page).to have_content "Edit your question"
-#       expect(page).to have_css("form")
-#     end
-
-#     context "with valid inputs" do
-#       it "should edit the question" do
-#         visit root_path
-#         click_on "Edit Question"
-#         fill_in "question[title]", with: edit_title
-#         fill_in "question[content]", with: edit_content
-#         click_on "Update Question"
-#         expect(page.status_code).to eq(200)
-#         expect(page).to_not have_content(test_title)
-#         expect(page).to_not have_content(test_content)
-#         expect(page).to have_content(edit_title)
-#         expect(page).to have_content(edit_content)
-#       end
-#     end
-
-#     context "with invalid inputs" do
-#       xit "should not edit the question with blank title" do
-#         visit root_path
-#         click_on "Edit Question"
-#         fill_in "question[content]", with: edit_content
-#         click_on "Update Question"
-#         expect(page).to_not have_content(test_title)
-#         expect(page).to_not have_content(test_content)
-#         expect(page).to have_content(edit_title)
-#         expect(page).to have_content(edit_content)
-#       end
-
-#       xit "should not edit the question with blank content" do
-#         visit root_path
-#         click_on "Edit Question"
-#         fill_in "question[title]", with: edit_title
-#         click_on "Update Question"
-#         expect(page).to_not have_content(test_title)
-#         expect(page).to_not have_content(test_content)
-#         expect(page).to have_content(edit_title)
-#         expect(page).to have_content(edit_content)
-#       end
-#     end
-
-#   end
-
+  describe "User can visit a doctor creation page" do
+    it "by clicking on create 'a new doctor'" do
+      visit doctors_path
+      click_on "Create a New Doctor"
+      expect(page.status_code).to eq(200)
+      expect(page).to have_css("form")
+    end
+    context "with valid parameters" do
+      it "should be able to create a new doctor if it doesn't already exist" do
+        expect{
+          visit new_doctor_path
+          fill_in "doctor[name]", with: doc_attr[:name]
+          fill_in "doctor[fax]", with: doc_attr[:fax]
+          fill_in "doctor[email]", with: doc_attr[:email]
+          fill_in "doctor[comments]", with: doc_attr[:comments]
+          click_on "Save Doctor"
+        }.to change{Doctor.count}.by(1)
+      end
+    end
+    context "with invalid parameters" do
+      it "should not able to create a new doctor if name field is empty" do
+        expect{
+          visit new_doctor_path
+          fill_in "doctor[fax]", with: doc_attr[:fax]
+          fill_in "doctor[email]", with: doc_attr[:email]
+          fill_in "doctor[comments]", with: doc_attr[:comments]
+          click_on "Save Doctor"
+        }.to change{Doctor.count}.by(0)
+      end
+      it "should not able to create a new doctor if fax field is empty" do
+        expect{
+          visit new_doctor_path
+          fill_in "doctor[name]", with: doc_attr[:name]
+          fill_in "doctor[email]", with: doc_attr[:email]
+          fill_in "doctor[comments]", with: doc_attr[:comments]
+          click_on "Save Doctor"
+        }.to change{Doctor.count}.by(0)
+      end
+      it "should not able to create a doctor with already existing name and fax combination" do
+        expect{
+          visit new_doctor_path
+          fill_in "doctor[name]", with: pre_created_doctor.name
+          fill_in "doctor[fax]", with: pre_created_doctor.fax
+          fill_in "doctor[email]", with: doc_attr[:email]
+          fill_in "doctor[comments]", with: doc_attr[:comments]
+          click_on "Save Doctor"
+        }.to change{Doctor.count}.by(0)
+      end
+    end
+  end
+  
+  describe "User can edit an existing question" do
+    context "with valid parameters" do
+      it "by clicking on edit" do
+        visit edit_doctor_path(pre_created_doctor)
+        p pre_created_doctor
+        fill_in "doctor[name]", with: doc_attr[:name]
+        fill_in "doctor[fax]", with: doc_attr[:fax]
+        fill_in "doctor[email]", with: doc_attr[:email]
+        fill_in "doctor[comments]", with: doc_attr[:comments]
+        click_on "Save Doctor"
+        expect(current_path).to eq doctor_path(pre_created_doctor) 
+        expect(page).to have_content doc_attr[:name]
+        expect(page).to have_content doc_attr[:fax]
+        expect(page).to have_content doc_attr[:email]
+        expect(page).to have_content doc_attr[:comments]
+      end
+    end
+  end
 end
