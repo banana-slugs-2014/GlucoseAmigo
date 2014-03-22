e262b9920981ce83e31156b364a3c96fb8ede41b
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-22 14:17:03 -0700

    Change controller specs and the controller to link it to diabetics

--- spec/controllers/doctor_spec.rb
+++ spec/controllers/doctor_spec.rb
@@ -2,31 +2,45 @@ require 'spec_helper'
 
 describe DoctorsController do
 	let(:doctor) { create :doctor }
-	let!(:pre_created_doctor) { create :doctor }
 	let(:doc_attr) { attributes_for :doctor }
+	let!(:pre_created_doctor) { create :doctor }
 	let!(:pre_created_doc_attr) { attributes_for :doctor }
+
+	before(:each) do
+		@account = create :account
+		@diabetic = @account.diabetics.new(	name: CoolFaker::Character.name,
+		 																		email: Faker::Internet.email,
+																			)
+		@diabetic.birth_date = {
+															year: (Date.today.year - 10 - rand(5)),
+															month: Date.today.month,
+															day: Date.today.day
+														}
+		@diabetic.save
+		request.session[:user_id] = @account.id
+	end
 
   context "#index" do
     it "should successfully render a page" do
-    	get :index
+    	get :index, diabetic_id: @diabetic.id
       expect(response).to be_success
     end
   end
 
   context "#show" do
     it "should successfully render a page" do
-    	get :show, id: doctor.id
+    	get :show, id: doctor.id, diabetic_id: @diabetic.id
       expect(response).to be_success
     end
     it "should display this specific doctor" do
-      get :show, id: doctor.id
+      get :show, id: doctor.id, diabetic_id: @diabetic.id
       expect(assigns(:doctor)).to eq(doctor)
     end
   end
 
   context "#new" do
     it "should successfully render a page" do
-    	get :new
+    	get :new, diabetic_id: @diabetic.id
       expect(response).to be_success
     end
   end
@@ -34,20 +48,20 @@ describe DoctorsController do
   context "#create" do
     it "should increase doctor count if doctor name and fax don't already exist" do
       expect{
-        post :create, doctor: doc_attr
+        post :create, doctor: doc_attr, diabetic_id: @diabetic.id
       }.to change{Doctor.count}.by(1)
     end
     it "should not increase doctor count if doctor name and fax already exists" do
         new_doctor = Doctor.create(doc_attr)
       expect{
-        post :create, doctor: { name: new_doctor.name, fax: new_doctor.fax }
+        post :create, doctor: { name: new_doctor.name, fax: new_doctor.fax }, diabetic_id: @diabetic.id
       }.to change{Doctor.count}.by(0)
     end
   end
 
   context "#edit" do
   	it "should render edit page with the right doctor" do
-  		get :edit, id: doctor.id
+  		get :edit, id: doctor.id, diabetic_id: @diabetic.id
   		expect(response).to be_success
   		expect(assigns(:doctor)).to eq(doctor)
   	end
@@ -57,7 +71,10 @@ describe DoctorsController do
   	it "should update target doctor info and redirect to show that doctor" do
   		new_email = doctor.email
   		new_comments = doctor.comments
-  		post :update, doctor: {email: new_email, comments: new_comments}, id: pre_created_doctor.id
+  		post :update, id: pre_created_doctor.id,
+										diabetic_id: @diabetic.id,
+										doctor: {email: new_email, comments: new_comments}
+
   		expect(pre_created_doctor.email).to eq(new_email)
   		expect(pre_created_doctor.comments).to eq(new_comments)
   	end
@@ -67,7 +84,10 @@ describe DoctorsController do
   		existing_fax = pre_created_doctor.fax
   		curr_name = doctor.name
   		curr_fax = doctor.fax
-  		post :update, doctor: {name: existing_name, fax: existing_fax}, id: doctor.id
+  		post :update, doctor: {name: existing_name, fax: existing_fax},
+										id: doctor.id,
+										diabetic_id: @diabetic.id
+
   		expect(doctor.name).to_not eq(existing_name)
   		expect(doctor.fax).to_not eq(existing_fax)
   		expect(doctor.name).to eq(curr_name)
