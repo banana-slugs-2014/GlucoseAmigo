8bd7ef70746edfbd4e05e55eb138e0957a7eea10
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-22 16:58:51 -0700

    Add a DB constraint on name and email, and switch age for birthday in the table and model

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -1,16 +1,20 @@
 class Diabetic < ActiveRecord::Base
-  attr_accessible :name, :email, :birth_date
+  attr_accessible :name, :email, :birthday
+  validates_presence_of :name, :email, :birthday
+  validates_format_of :email, :with => /^\w+[\.\w\-]*@\w+\.\w{2,5}$/
+  validates_format_of :birthday, :with => /^\d{4}-\d{1,2}-\d{1,2}$/ # We validate the format and that it is not an integer at the same time
   belongs_to :account
   belongs_to :doctor
-  validates_presence_of :name, :email, :age
-  validates_format_of :email, :with => /^\w+[\.\w\-]*@\w+\.\w{2,5}$/
-  validates_format_of :age, :with => /^\d+$/ # We validate the format and that it is not an integer at the same time
   has_many :records
 
-  def birth_date=(date)
-    bd = Date.parse(date.values.join('-'))
-    age = Date.today.year - bd.year
-    age -= 1 if Date.today < bd + age.years
-    self.age = age.to_s
+  before_save :convert_birthday
+
+  def convert_birthday
+    self.birthday = Date.parse(self.birthday.values.join('-'))
+  end
+
+  def age
+    age = Date.today.year - self.birthday.year
+    age -= 1 if Date.today < self.birthday.year + age.years
   end
 end
