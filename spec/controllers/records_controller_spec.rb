61dc986dbaad0393a464009fa44ca81d1e23801d
Chris Woodrich <cwoodrich@gmail.com>
2014-03-21 17:06:31 -0700

    Fix seeding issue in controller tests, continue work on views

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -1,22 +1,31 @@
 require 'spec_helper'
 
 describe RecordsController do
+  before :each do
+      @chris = Diabetic.create({name:'chris', email:'chris@dbc.com', age:'27'}, :without_protection => true)
+      @record1 = Record.create(glucose: '100', weight: '175', taken_at: (Time.now-500))
+      @chris.records << @record1
+    end
+
 
   context '#index' do
-    it "is ok" do
-      chris = Diabetic.create({name:'chris', email:'chris@dbc.com', age:'27'}, :without_protection => true)
-      record1 = Record.create(glucose: '100', weight: '175')
-      chris.records << record1
+    it "gets a users records" do
       get :index, diabetic_id: 1
       expect(response).to be_success
     end
   end
 
   context '#show' do
-    xit { returns_valid_response }
+    it 'shows a single record' do
+      get :show, diabetic_id: @chris.id, id: @record1.id
+      expect(response).to be_success
+    end
   end
+
   context '#edit' do
-    xit { returns_valid_response }
+    get :edit, diabetic_id: @chris.id, id: @record1.id
+    expect(response).to be_success
+
   end
   context '#new' do
     xit { returns_valid_response }
