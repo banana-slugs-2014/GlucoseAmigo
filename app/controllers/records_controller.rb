47da6a7d419cba1a109084689787a7766970b8da
Chris Woodrich <cwoodrich@gmail.com>
2014-03-22 11:16:18 -0700

    Add before filer to records controller, to get current user and DRY up controller methods

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,26 +1,27 @@
 class RecordsController < ApplicationController
 
-  def index
+  before_filter do
     @diabetic = Diabetic.find(params[:diabetic_id])
+  end
+
+
+  def index
     @records = @diabetic.records #take 10 at a time?
   end
 
   def show
-    @diabetic = Diabetic.find(params[:diabetic_id])
     @record = Record.find(params[:id])
     render partial: "shared/record", locals: {record: @record, diabetic: @diabetic}
   end
 
   def new
     @record = Record.new
-    @diabetic = Diabetic.find(params[:diabetic_id])
     #@diabetic = current_user #helper, to be written later
     render partial: 'shared/new_record', locals: {record: @record, diabetic: @diabetic}
   end
 
   def create
     @record = Record.create(params[:record])
-    @diabetic = Diabetic.find(params[:diabetic_id])
     if @record.save
       @diabetic.records << @record
       redirect_to diabetic_record_path(@diabetic, @record)
@@ -32,13 +33,11 @@ class RecordsController < ApplicationController
 
   def edit
     @record = Record.find(params[:id])
-    @diabetic = Diabetic.find(params[:diabetic_id])
     render partial: 'shared/edit_record', locals: {record: @record, diabetic: @diabetic}
   end
 
   def update
     @record = Record.find(params[:id])
-    @diabetic = Diabetic.find(params[:diabetic_id])
     if @record.update_attributes(params[:record])
       @record.save
       redirect_to diabetic_record_path(@diabetic, @record)
