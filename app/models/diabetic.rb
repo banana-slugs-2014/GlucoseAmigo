de757c6da95dfa87d6117231d2f796f6b72e216d
Chris Woodrich <cwoodrich@gmail.com>
2014-03-23 14:25:55 -0700

    Add data helper method to diabetic model in prep for graphing.

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -29,7 +29,19 @@ class Diabetic < ActiveRecord::Base
 
   def convert_birthday
     if self.birthday.is_a? Hash
-      self.birthday = Date.parse( self.birthday.values.join('-') )
+      self.birthday = Date.parse(self.birthday.values.join('-'))
     end
   end
+
+  def get_data_for_graph() #could take an argument for a range of data
+    glucose_graph_data = {}
+    weight_graph_data = {}
+    self.records.each do |record|
+      glucose_graph_data[record.taken_at] = record.glucose.to_i if record.glucose.to_i
+      weight_graph_data[record.taken_at] = record.weight.to_i if record.weight.to_i
+    end
+    [glucose_graph_data, weight_graph_data]
+  end
+
+
 end
