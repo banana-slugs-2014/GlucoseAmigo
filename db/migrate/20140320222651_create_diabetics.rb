a5bf4481c64c6ad0244a820a520724d41562de62
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-22 17:01:24 -0700

    Add db constraints for the rest of the migration

--- db/migrate/20140320222651_create_diabetics.rb
+++ db/migrate/20140320222651_create_diabetics.rb
@@ -2,8 +2,8 @@ class CreateDiabetics < ActiveRecord::Migration
   def change
     create_table :diabetics do |t|
       t.string :name, :email, :null => false
-      t.date :birthday
-      t.boolean :confirmed
+      t.date :birthday, :null => false
+      t.boolean :confirmed, :default => false
       t.belongs_to :doctor
       t.belongs_to :account
     end
