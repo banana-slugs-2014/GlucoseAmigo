4c4cb207411d6e8b22f493f61b8fdedc73cc3c5e
Chermaine Zimmerman <chermainespence@Chermaines-MacBook-Pro.local>
2014-03-22 18:51:30 -0700

    fixes doctor spec

--- spec/controllers/doctor_spec.rb
+++ spec/controllers/doctor_spec.rb
@@ -6,9 +6,6 @@ describe DoctorsController do
   let!(:pre_created_doctor) { create :doctor }
   let(:doc_attr) { attributes_for :doctor }
   let!(:pre_created_doc_attr) { attributes_for :doctor }
-  before(:each) do
-    request.session[:user_id] = account.id
-  end
 
 	before(:each) do
 		@account = create :account
@@ -27,7 +24,6 @@ describe DoctorsController do
   context "#index" do
     it "should successfully render a page" do
       request.env["HTTP_REFERER"] = new_session_path
-      request.session[:user_id] = account.id
       get :index, diabetic_id: @diabetic.id
       expect(response).to be_success
     end
@@ -35,7 +31,7 @@ describe DoctorsController do
     it 'redirects if logged_out' do
       request.env["HTTP_REFERER"] = new_session_path
       request.session.delete(:user_id)
-      get :index
+      get :index, diabetic_id: @diabetic.id
       expect(response).to be_redirect
     end
   end
@@ -53,7 +49,7 @@ describe DoctorsController do
     it 'redirects if logged_out' do
       request.env["HTTP_REFERER"] = new_session_path
       request.session.delete(:user_id)
-      get :show, id: doctor.id
+      get :show, id: doctor.id, diabetic_id: @diabetic.id
       expect(response).to be_redirect
     end
   end
@@ -67,7 +63,7 @@ describe DoctorsController do
     it 'redirects if logged_out' do
       request.env["HTTP_REFERER"] = new_session_path
       request.session.delete(:user_id)
-      get :new
+      get :new, diabetic_id: @diabetic.id
       expect(response).to be_redirect
     end
   end
@@ -121,7 +117,7 @@ describe DoctorsController do
   		expect(doctor.fax).to eq(curr_fax)
   	end
     it "should render edit page with the right doctor" do
-      get :edit, id: doctor.id
+      get :edit, id: doctor.id, diabetic_id: @diabetic.id
       expect(response).to be_success
       expect(assigns(:doctor)).to eq(doctor)
     end
@@ -129,7 +125,7 @@ describe DoctorsController do
      it 'redirects if logged_out' do
       request.env["HTTP_REFERER"] = new_session_path
       request.session.delete(:user_id)
-      get :edit, id: doctor.id
+      get :edit, id: doctor.id, diabetic_id: @diabetic.id
       expect(response).to be_redirect
     end
   end
@@ -138,7 +134,7 @@ describe DoctorsController do
     it "should update target doctor info and redirect to show that doctor" do
       new_email = doctor.email
       new_comments = doctor.comments
-      post :update, doctor: {email: new_email, comments: new_comments}, id: pre_created_doctor.id
+      post :update, doctor: {email: new_email, comments: new_comments}, id: pre_created_doctor.id, diabetic_id: @diabetic.id
       expect(pre_created_doctor.email).to eq(new_email)
       expect(pre_created_doctor.comments).to eq(new_comments)
     end
@@ -148,7 +144,7 @@ describe DoctorsController do
       existing_fax = pre_created_doctor.fax
       curr_name = doctor.name
       curr_fax = doctor.fax
-      post :update, doctor: {name: existing_name, fax: existing_fax}, id: doctor.id
+      post :update, doctor: {name: existing_name, fax: existing_fax}, id: doctor.id, diabetic_id: @diabetic.id
       expect(doctor.name).to_not eq(existing_name)
       expect(doctor.fax).to_not eq(existing_fax)
       expect(doctor.name).to eq(curr_name)
