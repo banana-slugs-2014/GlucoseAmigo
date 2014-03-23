d4d13d643937eb5ee39fa8646786f325680bbc0f
Chermaine Zimmerman <chermainespence@chermaines-mbp.att.net>
2014-03-23 11:39:35 -0700

    adds links for edit page

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -1,35 +1,36 @@
 class Diabetic < ActiveRecord::Base
   attr_accessible :name, :email, :birthday
 
-  # Validations
-  validates_presence_of :name, :email, :birthday
-  validates_format_of :email, :with => /^\w+[\.\w\-]*@\w+\.\w{2,5}$/
-  validate :birthday_cant_be_in_the_future
+#   # Validations
+#   validates_presence_of :name, :email, :birthday
+#   validates_format_of :email, :with => /^\w+[\.\w\-]*@\w+\.\w{2,5}$/
+#   validate :birthday_cant_be_in_the_future
 
   # Associations
   belongs_to :account
   belongs_to :doctor
   has_many :records
   has_one :preference
-
-  after_initialize :convert_birthday
-
-  def age
-    age = Date.today.year - self.birthday.year
-    age -= 1 if Date.today < self.birthday.year + age.years
-  end
-
-  private
-
-  def birthday_cant_be_in_the_future
-    if birthday.present? && birthday >= Date.today
-      errors.add(:birthday, "must be before today")
-    end
-  end
-
-  def convert_birthday
-    if self.birthday.is_a? Hash
-      self.birthday = Date.parse( self.birthday.values.join('-') )
-    end
-  end
 end
+
+#   after_initialize :convert_birthday
+
+#   def age
+#     age = Date.today.year - self.birthday.year
+#     age -= 1 if Date.today < self.birthday.year + age.years
+#   end
+
+#   private
+
+#   def birthday_cant_be_in_the_future
+#     if birthday.present? && birthday >= Date.today
+#       errors.add(:birthday, "must be before today")
+#     end
+#   end
+
+#   def convert_birthday
+#     if self.birthday.is_a? Hash
+#       self.birthday = Date.parse( self.birthday.values.join('-') )
+#     end
+#   end
+# end
