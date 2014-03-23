b48d50b85162887432f2a5951a981bc834ce6bf2
Tim <tyau52@gmail.com>
2014-03-23 10:29:00 -0700

    Undo a change to the file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,6 @@ GlucoseAmigo::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-#   config.assets.debug = true
+  config.assets.debug = true
 
 end
