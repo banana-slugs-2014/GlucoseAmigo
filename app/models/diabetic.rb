b7a9e402894fe648a0eb8087ae1c658bfe59e36f
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-22 17:20:03 -0700

    Change before_save to after_initialize and put a validation for future dates

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -1,20 +1,33 @@
 class Diabetic < ActiveRecord::Base
   attr_accessible :name, :email, :birthday
+
+  # Validations
   validates_presence_of :name, :email, :birthday
   validates_format_of :email, :with => /^\w+[\.\w\-]*@\w+\.\w{2,5}$/
+  validate :birthday_cant_be_in_the_future
+
+  # Associations
   belongs_to :account
   belongs_to :doctor
   has_many :records
   has_one :preference
 
-  before_save :convert_birthday
-
-  def convert_birthday
-    self.birthday = Date.parse(self.birthday.values.join('-'))
-  end
+  after_initialize :convert_birthday
 
   def age
     age = Date.today.year - self.birthday.year
     age -= 1 if Date.today < self.birthday.year + age.years
+  end
+
+  private
+
+  def birthday_cant_be_in_the_future
+    if birthday.present? && birthday >= Date.today
+      errors.add(:birthday, "must be before today")
+    end
+  end
+
+  def convert_birthday
+    self.birthday = Date.parse(self.birthday.values.join('-'))
   end
 end
