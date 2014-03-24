b62bf7fa952420a70a3c471dd2353a04c9a9d350
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-23 23:00:38 -0700

    Add a user login in JS

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,12 @@
 GlucoseAmigo = {}
 $(document).ready(function(){
+  GlucoseAmigo.user = new User();
+  GlucoseAmigo.user.listenForLogin();
   GlucoseAmigo.view = new View();
   GlucoseAmigo.controller = new Controller(GlucoseAmigo.view)
   GlucoseAmigo.view.reloadPage();
   Binder.bind(GlucoseAmigo.controller);
+
+  
+  $('body').trigger('login', userid)
 });
