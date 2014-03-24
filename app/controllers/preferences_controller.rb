65ea4746bff05a49463a414f8d481442285a08e4
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-24 10:23:37 -0700

    Full working routes to the dashboard

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -5,6 +5,7 @@ class PreferencesController < ApplicationController
     #(redirect_to edit_diabetic_preference_path(@diabetic.id, @diabetic.preference)) if @diabetic.preference
     @preference = Preference.new
     render :partial => 'shared/new_preference', locals: {
+                                                          title: "Create a doctor",
                                                           diabetic: @diabetic,
                                                           preference: @preference
                                                         }
@@ -18,21 +19,31 @@ class PreferencesController < ApplicationController
       preference = Preference.create(params[:preference])
       @diabetic.preference = preference
     end
-    render :json {
-        ok: true,
-        path: diabetic_preference_path(@diabetic, @diabetic.preference),
-        alert: preference.errors.full_message
-      }
+    redirect_to account_path(current_account)
+    # render :json => {
+    #     ok: true,
+    #     path: diabetic_preference_path(@diabetic),
+    #     alert: @diabetic.preference.errors.full_message
+    #   }
   end
 
   def show
     @diabetic = Diabetic.find(params[:diabetic_id])
     @preference = Preference.find(params[:id])
+    render :partial => 'shared/show_preference', locals: {
+                          diabetic: @diabetic,
+                          preference: @preference
+                        }
   end
 
   def edit
     @diabetic = Diabetic.find(params[:diabetic_id])
     @preference = Preference.find(params[:id])
+    render :partial => 'shared/new_preference', locals: {
+                                                          title: "Modify a doctor",
+                                                          diabetic: @diabetic,
+                                                          preference: @preference
+                                                        }
   end
 
   def update
@@ -42,7 +53,11 @@ class PreferencesController < ApplicationController
     else
       @diabetic.preference.create(params[:preference])
     end
-    redirect_to diabetic_preference_path(@diabetic, @diabetic.preference)
+    render :json => {
+        ok: true,
+        path: diabetic_preference_path(@diabetic, @diabetic.preference),
+        alert: @diabetic.preference.errors.full_message
+      }
   end
 
 end
