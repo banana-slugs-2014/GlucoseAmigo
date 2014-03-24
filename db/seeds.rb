a8d521acfe695c5ff03263ddf6f4b386d0a4bd86
Armando <apprentice@dbc20.local>
2014-03-23 17:38:25 -0700

    Add ultimate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do |i|
+
+  Account.create(username: Faker::Name.name,
+                 password: "password",
+                 password_confirmation: "password",
+                 email: Faker::Internet.email)
+
+  Doctor.create(name: Faker::Name.name,
+                fax: "+1-800-555-555#{i}",
+                comments: Faker::Lorem.sentence,
+                email: Faker::Internet.email)
+
+
+  Account.last.diabetics.create(name: Faker::Name.name,
+                                email: Faker::Internet.email,
+                                birthday: Date.today - rand(2000))
+
+
+  Diabetic.last.preference = Preference.create(reminders: true,
+                                   frequency: (i + 1))
+  3.times do
+    7.times do |j|
+      Diabetic.last.records.create(weight: "20#{i}",
+                                   glucose: rand(75..100).to_s,
+                                   taken_at: Date.today - (j) )
+    end
+  end
+end
