00f055987170c9da8a88bab974ab01f4e8531822
Chermaine Zimmerman <chermainespence@Chermaines-MacBook-Pro.local>
2014-03-24 18:26:59 -0700

    working graph with plot bands

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -16,46 +16,63 @@ $(document).on('ready', function() {
 
 function graph(response) {
     $('#container').highcharts({
-        chart: {
-            zoomType: 'xy'
-        },
-        title: {
-            text: 'Weekly Glucose and Weight'
-        },
-        subtitle: {
-            text: response.diabetic.name
-        },
-        xAxis: [{
-            categories: response.days
-        }],
-        yAxis: [{ // Primary yAxis
-            labels: {
-                format: '{value}mg/dL',
-                style: {
-                    color: '#89A54E'
-                }
+            chart: {
+                zoomType: 'xy'
             },
             title: {
-                text: 'Glucose',
-                style: {
-                    color: '#89A54E'
-                }
-            }
-        }, { // Secondary yAxis
-            title: {
-                text: 'Weight',
-                style: {
-                    color: '#4572A7'
-                }
+                text: 'Weekly Glucose and Weight'
             },
-            labels: {
-                format: '{value} lbs',
-                style: {
-                    color: '#4572A7'
-                }
+            subtitle: {
+                text: response.diabetic.name
             },
-            opposite: true
-        }],
+            xAxis: [{
+                categories: response.days
+            }],
+            yAxis: [{ // Primary yAxis
+                    labels: {
+                        format: '{value}mg/dL',
+                        style: {
+                            color: '#89A54E'
+                        }
+                    },
+                    title: {
+                        text: 'Glucose',
+                        style: {
+                            color: '#89A54E'
+                        }
+                    }
+
+                }, { // Secondary yAxis
+                    title: {
+                        text: 'Weight',
+                        style: {
+                            color: '#4572A7'
+                        }
+                    },
+                    min: 0,
+                    minorGridLineWidth: 0,
+                    gridLineWidth: 0,
+                    alternateGridColor: null,
+                    plotBands: [{
+                            from: 70.0,
+                            to: 130.0,
+                            color: 'rgba(68,170,213,0.1)',
+                            label: {
+                                text: 'Normal Glucose Levels',
+                                style: {
+                                    color: '#606060'
+                                }
+                            }
+                        },
+                    }],
+                labels: {
+                    format: '{value} lbs',
+                    style: {
+                        color: '#4572A7'
+                    }
+                },
+                opposite: true
+            }],
         tooltip: {
             shared: true
         },
