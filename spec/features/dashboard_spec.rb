8ab5225cb776fd0d5bc2d5f061537f949a202bf7
Tim <tyau52@gmail.com>
2014-03-25 11:51:53 -0700

    Add more tests

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "dashboard", js: true  do
 	let(:doctor) { create :doctor }
+	let(:doctor_attr) { attributes_for :doctor}
 	let(:diabetic) { create :diabetic }
 	let(:diabetic_attr) {  attributes_for :diabetic }
 	let(:record) { create :record }
@@ -64,6 +65,7 @@ describe "dashboard", js: true  do
 
 		it "user can add a diabetic" do
 			find("option[value='Account: #{@account.username}']").click
+			#Adds a diabetic
 			click_on "Add a Diabetic"
 			wait_for_ajax
 			expect{
@@ -75,11 +77,29 @@ describe "dashboard", js: true  do
 				click_on "Create"
 				wait_for_ajax
 			}.to change{Diabetic.count}.by(1)
-			#p Diabetic.count
-			#p Diabetic.count
-			#expect{click_on "Create"}.to change{@account.diabetics.count}.by(1)
 			expect(@account.diabetics.last.name).to eq(diabetic_attr[:name])
 			expect(@account.diabetics.last.email).to eq(diabetic_attr[:email])
+
+			#Adds a doctor to the diabetic
+			fill_in "doctor[name]", with: doctor_attr[:name]
+			fill_in "doctor[fax]", with: doctor_attr[:fax]
+			fill_in "doctor[email]", with: doctor_attr[:email]
+			fill_in "doctor[comments]", with: doctor_attr[:comments]
+			click_on "Save Doctor"
+			wait_for_ajax
+			expect(@account.diabetics.last.doctor.name).to eq(doctor_attr[:name])
+			expect(@account.diabetics.last.doctor.fax).to eq(doctor_attr[:fax])
+			expect(@account.diabetics.last.doctor.email).to eq(doctor_attr[:email])
+			expect(@account.diabetics.last.doctor.comments).to eq(doctor_attr[:comments])
+
+			#adds preference to diabetic
+			choose "preference_reminders_false"
+			find("option[value='2']").click
+			click_on "Save Preferences"
+			wait_for_ajax
+			expect(@account.diabetics.last.preference.reminders).to eq(false)
+			expect(@account.diabetics.last.preference.frequency).to eq(2)
+
 		end
 
 		it "user can cancel the add a diabetic" do
@@ -90,6 +110,7 @@ describe "dashboard", js: true  do
 			wait_for_ajax
 			expect(page.body).to have_content("Dashboard")
 		end
+
 	end
 
 	context "with diabetics" do
@@ -136,17 +157,43 @@ describe "dashboard", js: true  do
 			it "can edit preferences" do
 				click_on "Add Preferences"
 				wait_for_ajax
-				#@diabetic
+				choose "preference_reminders_false"
+				find("option[value='2']").click
+				click_on "Save Preferences"
+				wait_for_ajax
+				expect(@diabetic.preference.reminders).to eq(false)
+				expect(@diabetic.preference.frequency).to eq(2)
 			end
 
 			it "can edit user info" do
 				click_on "Edit User Info"
 				wait_for_ajax
+				expect{
+					fill_in "diabetic[name]", with: diabetic_attr[:name]
+					fill_in "diabetic[email]", with: diabetic_attr[:email]
+					within '#diabetic_birthday_1i' do
+						find("option[value='2009']").click
+					end
+					click_on "Save"
+					wait_for_ajax
+				}.to change{Diabetic.count}.by(0)
+				expect(@account.diabetics.last.name).to eq(diabetic_attr[:name])
+				expect(@account.diabetics.last.email).to eq(diabetic_attr[:email])
 			end
 
 			it "can edit doctor info" do
 				click_on "Edit Doctor Info"
 				wait_for_ajax
+				fill_in "doctor[name]", with: doctor_attr[:name]
+				fill_in "doctor[fax]", with: doctor_attr[:fax]
+				fill_in "doctor[email]", with: doctor_attr[:email]
+				fill_in "doctor[comments]", with: doctor_attr[:comments]
+				click_on "Save Doctor"
+				wait_for_ajax
+				expect(@diabetic.doctor.name).to eq(doctor_attr[:name])
+				expect(@diabetic.doctor.fax).to eq(doctor_attr[:fax])
+				expect(@diabetic.doctor.email).to eq(doctor_attr[:email])
+				expect(@diabetic.doctor.comments).to eq(doctor_attr[:comments])
 			end
 
 		end
