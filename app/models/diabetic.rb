1e8c4389d240241ea8773850a545788cd1db1aef
Chris Woodrich <cwoodrich@gmail.com>
2014-03-25 16:38:12 -0700

    Added seperate methods for data retrieval based on type opf request

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -23,20 +23,28 @@ class Diabetic < ActiveRecord::Base
 
 
   def get_data_for_graph(number_of_days=7)
-    collection = self.records.where(taken_at: (Time.now.midnight - number_of_days.days)..Time.now.midnight)
-    get_data(collection)
+    collection = load_records(number_of_days)
+    get_data(collection, '%a %m/%d')
   end
 
+  def get_data_for_pdf(number_of_days=14)
+    collection = load_records(number_of_days)
+    get_data(collection, '%m/%d/%y')
+  end
 
   private
 
-  def get_data(collection)
+  def load_records(number_of_days=7)
+    self.records.where(taken_at: (Time.now.midnight - number_of_days.days)..Time.now.midnight)
+  end
+
+  def get_data(collection, format='')
     #could take an argument for a range of data
      glucose_graph_data = {}
      weight_graph_data = {}
      collection.each do |record|
-       glucose_graph_data[record.taken_at.localtime.strftime('%a %m/%d').to_s] = record.glucose.to_i if record.glucose.to_i
-       weight_graph_data[record.taken_at.localtime.strftime('%a %m/%d').to_s] = record.weight.to_i if record.weight.to_i
+       glucose_graph_data[record.taken_at.localtime.strftime('#{format}').to_s] = record.glucose.to_i if record.glucose.to_i
+       weight_graph_data[record.taken_at.localtime.strftime('#{format}').to_s] = record.weight.to_i if record.weight.to_i
      end
      [glucose_graph_data, weight_graph_data]
   end
