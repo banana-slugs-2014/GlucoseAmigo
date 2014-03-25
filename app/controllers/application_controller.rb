a701d79c533291f92877b78fb21c727d2a7a6088
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-24 20:30:00 -0700

    Change the ApplicationHelper authentication methods into an AuthenticationHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include ApplicationHelper
+  include AuthenticationHelper
 
   protected
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def current_account
-    Account.find(session[:account_id])
-  end
-
-  def logged_in?
-    !!session[:account_id]
-  end
-
-  def login account
-    session[:account_id] = account.id
-  end
 end
