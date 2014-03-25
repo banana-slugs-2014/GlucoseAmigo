1acc3a1fb15cbea85d5a9a14baafba606566fb70
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-24 23:57:07 -0700

    Move the menu to the correct location

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def get
     choices = params[:menu_choice].split(':')
     @diabetic = Diabetic.find(choices[1].split('--')[1]) if choices[0] == 'Diabetic'
-    render :partial => 'shared/menu', :locals => {
+    render :partial => 'dashboard/menu', :locals => {
                                                     diabetic: @diabetic,
                                                     account: current_account
                                                   }
