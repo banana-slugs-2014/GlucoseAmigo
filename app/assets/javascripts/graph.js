3d79a92a5c89dd6962a783129dacd13460ea5b50
Chermaine Zimmerman <chermainespence@Chermaines-MacBook-Pro.local>
2014-03-25 10:45:51 -0700

    adds ajax for graph

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,78 +1,62 @@
-$(document).on('ready', function() {
-    $('.graph').on('click', function(event) {
-        event.preventDefault();
-        $.ajax({
-            type: 'get',
-            url: '/accounts/1/diabetics/2',
-            success: function(response) {
-                console.log(response)
-                graph(response);
-
-            }
-
-        })
-    })
-})
-
 function graph(response) {
+
     $('#container').highcharts({
-            chart: {
-                zoomType: 'xy'
+        chart: {
+            zoomType: 'xy'
+        },
+        title: {
+            text: 'Weekly Glucose and Weight'
+        },
+        subtitle: {
+            text: response.diabetic.name
+        },
+        xAxis: [{
+            categories: response.days
+        }],
+        yAxis: [{ // Primary yAxis
+            labels: {
+                format: '{value}mg/dL',
+                style: {
+                    color: 'red'
+                }
             },
             title: {
-                text: 'Weekly Glucose and Weight'
+                text: 'Glucose',
+                style: {
+                    color: 'red'
+                }
             },
-            subtitle: {
-                text: response.diabetic.name
-            },
-            xAxis: [{
-                categories: response.days
-            }],
-            yAxis: [{ // Primary yAxis
-                    labels: {
-                        format: '{value}mg/dL',
-                        style: {
-                            color: '#89A54E'
-                        }
-                    },
-                    title: {
-                        text: 'Glucose',
-                        style: {
-                            color: '#89A54E'
-                        }
-                    }
-
-                }, { // Secondary yAxis
-                    title: {
-                        text: 'Weight',
-                        style: {
-                            color: '#4572A7'
-                        }
-                    },
-                    min: 0,
-                    minorGridLineWidth: 0,
-                    gridLineWidth: 0,
-                    alternateGridColor: null,
-                    plotBands: [{
-                            from: 70.0,
-                            to: 130.0,
-                            color: 'rgba(68,170,213,0.1)',
-                            label: {
-                                text: 'Normal Glucose Levels',
-                                style: {
-                                    color: '#606060'
-                                }
-                            }
-                        },
-                    }],
-                labels: {
-                    format: '{value} lbs',
+            min: 0,
+            minorGridLineWidth: 0,
+            gridLineWidth: 0,
+            alternateGridColor: null,
+            plotBands: [{
+                from: 70.0,
+                to: 130.0,
+                color: 'lightblue',
+                label: {
+                    text: 'Normal Glucose Levels',
                     style: {
-                        color: '#4572A7'
+                        color: '#606060'
                     }
-                },
-                opposite: true
+                }
+
             }],
+        }, { // Secondary yAxis
+            title: {
+                text: 'Weight',
+                style: {
+                    color: '#4572A7'
+                }
+            },
+            labels: {
+                format: '{value} lbs',
+                style: {
+                    color: '#4572A7'
+                }
+            },
+            opposite: true
+        }],
         tooltip: {
             shared: true
         },
@@ -98,7 +82,7 @@ function graph(response) {
 
         }, {
             name: 'Glucose',
-            color: '#89A54E',
+            color: 'red',
             type: 'spline',
             data: response.glucose,
             tooltip: {
