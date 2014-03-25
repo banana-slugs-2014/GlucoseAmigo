03744adc8e54d3dceb359f1fc439684b5f7497c1
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-24 19:29:10 -0700

    Refactor Diabetics Controller

--- app/controllers/diabetics_controller.rb
+++ app/controllers/diabetics_controller.rb
@@ -1,6 +1,7 @@
 class DiabeticsController < ApplicationController
 
-  before_filter :redirect_if_logged_out, except: [:new]
+  before_filter :redirect_if_logged_out, except: [ :new ]
+  before_filter :load_diabetic, except: [ :create, :new ]
 
   def new
     account = current_account
@@ -12,9 +13,8 @@ class DiabeticsController < ApplicationController
   def create
     diabetic = Diabetic.new(params[:diabetic])
     diabetic.account = current_account
-    if diabetic.valid?
+    if diabetic.save
       ok = true
-      diabetic.save
       #DiabeticMailer.welcome_email(diabetic).deliver
       path = new_diabetic_doctor_path(diabetic_id: diabetic.id)
     else
@@ -28,7 +28,6 @@ class DiabeticsController < ApplicationController
   end
 
   def edit_menu
-    @diabetic = Diabetic.find(params[:id])
     render :partial => "shared/diabetic_edit_page", :locals => {
                                                         diabetic: @diabetic,
                                                         account: current_account
@@ -36,33 +35,31 @@ class DiabeticsController < ApplicationController
   end
 
   def edit
-    diabetic = Diabetic.find(params[:id])
     render :partial => "shared/edit_diabetic", :locals => {
-                                                        diabetic: diabetic,
-                                                        account: diabetic.account
+                                                        diabetic: @diabetic,
+                                                        account: @diabetic.account
                                                       }
   end
 
   def update
-    diabetic = Diabetic.find(params[:id])
-    diabetic.update_attributes(params[:diabetic])
-    if diabetic.valid?
-      ok = true
-      diabetic.save
-    end
+    @diabetic.update_attributes(params[:diabetic])
+    @diabetic.save
     redirect_to :back
   end
 
   def destroy
-    Diabetic.find(params[:id]).destroy
+    @diabetic.destroy
     redirect_to new_account_diabetic_path(account_id: current_account.id)
   end
 
   def get_graph_data
-    @diabetic = Diabetic.find(params[:id])
-    @data = diabetic.get_data_for_graph()
+    @data = @diabetic.get_data_for_graph()
     @data.to_json
   end
 
+  private
+  def load_diabetic
+    @diabetic = Diabetic.find(params[:id])
+  end
 
 end
