9280597cb76c60262b06b2e6dd8538dfa76f0de0
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-25 16:19:16 -0700

    Remake code that was apparently deleted for logout. Fix  #81

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -11,7 +11,10 @@ Controller.prototype = {
         this.view.addAlert(alert);
     },
     logout: function() {
-        Ajax.logout(this.user.uId, this.loadNextPage.bind(this));
+        Ajax.logout(this.user.uId, this.redirectTo.bind(this));
+    },
+    redirectTo: function(data){
+        window.location.replace(data.path);
     },
     addGraph: function(data) {
         Ajax.get(data.url, graph)
