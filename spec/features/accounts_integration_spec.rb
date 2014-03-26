9815cf74ea61279ef60acbbccc1da103cbd6eca1
Tim <tyau52@gmail.com>
2014-03-26 16:45:07 -0700

    Add spec to test signup with pre-existing account information that needs to be unique

--- spec/features/accounts_integration_spec.rb
+++ spec/features/accounts_integration_spec.rb
@@ -2,66 +2,101 @@ require 'spec_helper'
 
 
 describe "Accounts", :js => true do
-  let!(:account) {create :account}
+  let(:account) {create :account}
+  let!(:pre_existing_account) {create :account}
   let(:diabetic) {create :diabetic}
   let(:diabetic_attr) {  attributes_for :diabetic }
   let(:doctor_attr) {  attributes_for :doctor }
 
   describe "full account creation process" do
-    it 'allows user to create an account with sign up' do
-      visit root_path
-      click_on "Sign Up"
-      wait_for_ajax
-      expect{
-        fill_in "Username", with: 'business'
-        fill_in "Email", with: 'business@business.com'
-        fill_in "Password", with: 'business'
-        fill_in "Password Confirmation", with: 'business'
-        click_on "Sign up"
-        wait_for_ajax
-      }.to change{Account.count}.by(1)
-      expect(page).to have_content 'Add a diabetic'
-      expect(Account.last.username).to eq('business')
-      expect(Account.last.email).to eq('business@business.com')
 
-      new_account = Account.last
-
-      expect{
-        fill_in "diabetic[name]", with: diabetic_attr[:name]
-        fill_in "diabetic[email]", with: diabetic_attr[:email]
-        find("option[value='2009']").click
-        click_on "Create"
+    context "all valid, none-existing input" do
+      it 'allows user to create an account with sign up with valid inputs' do
+        visit root_path
+        click_on "Sign Up"
         wait_for_ajax
-      }.to change{Diabetic.count}.by(1)
-      expect(new_account.diabetics.last.name).to eq(diabetic_attr[:name])
-      expect(new_account.diabetics.last.email).to eq(diabetic_attr[:email])
-      expect(page).to have_content 'Create a Doctor'
+        expect{
+          fill_in "Username", with: 'business'
+          fill_in "Email", with: 'business@business.com'
+          fill_in "Password", with: 'business'
+          fill_in "Password Confirmation", with: 'business'
+          click_on "Sign up"
+          wait_for_ajax
+        }.to change{Account.count}.by(1)
+        expect(page).to have_content 'Add a diabetic'
+        expect(Account.last.username).to eq('business')
+        expect(Account.last.email).to eq('business@business.com')
 
-      expect{
-        fill_in "doctor[name]", with: doctor_attr[:name]
-        fill_in "doctor[fax]", with: doctor_attr[:fax]
-        fill_in "doctor[email]", with: doctor_attr[:email]
-        fill_in "doctor[comments]", with: doctor_attr[:comments]
-        click_on "Save Doctor"
-        wait_for_ajax
-      }.to change{Doctor.count}.by(1)
-      expect(new_account.reload.diabetics.last.doctor.name).to eq(doctor_attr[:name])
-      expect(new_account.reload.diabetics.last.doctor.fax).to eq(doctor_attr[:fax])
-      expect(new_account.reload.diabetics.last.doctor.email).to eq(doctor_attr[:email])
-      expect(new_account.reload.diabetics.last.doctor.comments).to eq(doctor_attr[:comments])
-      expect(page).to have_content 'preferences'
+        new_account = Account.last
 
-      expect{
-        find("option[value='2']").click
-        click_on "Save"
+        expect{
+          fill_in "diabetic[name]", with: diabetic_attr[:name]
+          fill_in "diabetic[email]", with: diabetic_attr[:email]
+          find("option[value='2009']").click
+          click_on "Create"
+          wait_for_ajax
+        }.to change{Diabetic.count}.by(1)
+        expect(new_account.diabetics.last.name).to eq(diabetic_attr[:name])
+        expect(new_account.diabetics.last.email).to eq(diabetic_attr[:email])
+        expect(page).to have_content 'Create a Doctor'
+
+        expect{
+          fill_in "doctor[name]", with: doctor_attr[:name]
+          fill_in "doctor[fax]", with: doctor_attr[:fax]
+          fill_in "doctor[email]", with: doctor_attr[:email]
+          fill_in "doctor[comments]", with: doctor_attr[:comments]
+          click_on "Save Doctor"
+          wait_for_ajax
+        }.to change{Doctor.count}.by(1)
+        expect(new_account.reload.diabetics.last.doctor.name).to eq(doctor_attr[:name])
+        expect(new_account.reload.diabetics.last.doctor.fax).to eq(doctor_attr[:fax])
+        expect(new_account.reload.diabetics.last.doctor.email).to eq(doctor_attr[:email])
+        expect(new_account.reload.diabetics.last.doctor.comments).to eq(doctor_attr[:comments])
+        expect(page).to have_content 'preferences'
+
+        expect{
+          find("option[value='2']").click
+          click_on "Save"
+          wait_for_ajax
+        }.to change{Preference.count}.by(1)
+        expect(new_account.reload.diabetics.last.preference.reminders).to eq(true)
+        expect(new_account.reload.diabetics.last.preference.frequency).to eq(2)
+        expect(page).to have_content 'Dashboard'
+      end
+    end
+
+    context "with pre-existing inputs" do
+      it "with none-unique account name" do
+        visit root_path
+        click_on "Sign Up"
         wait_for_ajax
-      }.to change{Preference.count}.by(1)
-      expect(new_account.reload.diabetics.last.preference.reminders).to eq(true)
-      expect(new_account.reload.diabetics.last.preference.frequency).to eq(2)
-      expect(page).to have_content 'Dashboard'
+        expect{
+          fill_in "Username", with: pre_existing_account.username
+          fill_in "Email", with: 'business@business.com'
+          fill_in "Password", with: 'business'
+          fill_in "Password Confirmation", with: 'business'
+          click_on "Sign up"
+          wait_for_ajax
+        }.to change{Account.count}.by(0)
+        expect(page).to have_content 'Username has already been taken'
+      end
+      it "with none-unique account email" do
+        visit root_path
+        click_on "Sign Up"
+        wait_for_ajax
+        expect{
+          fill_in "Username", with: 'business'
+          fill_in "Email", with: pre_existing_account.email
+          fill_in "Password", with: 'business'
+          fill_in "Password Confirmation", with: 'business'
+          click_on "Sign up"
+          wait_for_ajax
+        }.to change{Account.count}.by(0)
+      end
 
 
     end
+
   end
 
 
