f0d5f0f6dbaa7f85949596fc04154b26d7246e5b
Tim <tyau52@gmail.com>
2014-03-26 15:54:45 -0700

    Fix major issue of username not unique, resulting when anyone logs in, they always log into the first account with said username

--- db/migrate/20140320222513_create_accounts.rb
+++ db/migrate/20140320222513_create_accounts.rb
@@ -1,9 +1,10 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
-      t.string :username
+      t.string :username, unique: true
       t.string :password_digest
-      t.string :email
+      t.string :email, unique: true
     end
   end
 end
+
