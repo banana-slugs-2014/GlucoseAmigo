f6dc487277feb2387971cfccc28457a41c2140cd
Tim <tyau52@gmail.com>
2014-03-26 14:49:13 -0700

    Remove tests for unused paths, fix some pending tests

--- spec/controllers/doctors_controller_spec.rb
+++ spec/controllers/doctors_controller_spec.rb
@@ -15,49 +15,10 @@ describe DoctorsController do
 		stub_current_account(account)
 	end
 
-  context "#index" do
-    xit "should successfully render a page" do
-      get :index, diabetic_id: diabetic.id
-      expect(response).to be_success
-    end
-
-    xit 'redirects if logged_out' do
-      request.env["HTTP_REFERER"] = new_session_path
-      request.session.delete(:account_id)
-      get :index, diabetic_id: diabetic.id
-      expect(response).to be_redirect
-    end
-  end
-
-  context "#show" do
-    xit "should successfully render a page" do
-    	get :show, id: doctor.id, diabetic_id: diabetic.id
-      expect(response).to be_success
-    end
-    xit "should display this specific doctor" do
-      get :show, id: doctor.id, diabetic_id: diabetic.id
-      expect(assigns(:doctor)).to eq(doctor)
-    end
-
-    xit 'redirects if logged_out' do
-      request.env["HTTP_REFERER"] = new_session_path
-      request.session.delete(:account_id)
-      get :show, id: doctor.id, diabetic_id: diabetic.id
-      expect(response).to be_redirect
-    end
-  end
-
   context "#new" do
     it "should successfully render a page" do
     	get :new, diabetic_id: diabetic.id
       expect(response).to be_success
-    end
-
-    xit 'redirects if logged_out' do
-      request.env["HTTP_REFERER"] = new_session_path
-      request.session.delete(:account_id)
-      get :new, diabetic_id: diabetic.id
-      expect(response).to be_redirect
     end
   end
 
@@ -81,46 +42,6 @@ describe DoctorsController do
   		expect(response).to be_success
   		expect(assigns(:doctor)).to eq(doctor)
   	end
-  end
-
-  context "#update" do
-  	xit "should update target doctor info and redirect to show that doctor" do
-  		new_email = doctor.email
-  		new_comments = doctor.comments
-  		post :update, id: pre_created_doctor.id,
-										diabetic_id: diabetic.id,
-										doctor: {email: new_email, comments: new_comments}
-
-  		expect(pre_created_doctor.email).to eq(new_email)
-  		expect(pre_created_doctor.comments).to eq(new_comments)
-  	end
-
-  	it "should not update target doctor info into an existing name-fax pair" do
-  		existing_name = pre_created_doctor.name
-  		existing_fax = pre_created_doctor.fax
-  		curr_name = doctor.name
-  		curr_fax = doctor.fax
-  		post :update, doctor: {name: existing_name, fax: existing_fax},
-										id: doctor.id,
-										diabetic_id: diabetic.id
-
-  		expect(doctor.name).to_not eq(existing_name)
-  		expect(doctor.fax).to_not eq(existing_fax)
-  		expect(doctor.name).to eq(curr_name)
-  		expect(doctor.fax).to eq(curr_fax)
-  	end
-    it "should render edit page with the right doctor" do
-      get :edit, id: doctor.id, diabetic_id: diabetic.id
-      expect(response).to be_success
-      expect(assigns(:doctor)).to eq(doctor)
-    end
-
-     xit 'redirects if logged_out' do
-      request.env["HTTP_REFERER"] = new_session_path
-      request.session.delete(:account_id)
-      get :edit, id: doctor.id, diabetic_id: diabetic.id
-      expect(response).to be_redirect
-    end
   end
 
   context "#update" do
