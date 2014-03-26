89da5ad0f7e10bfa800926be72a442d916ecc3f2
Chermaine Zimmerman <chermainespence@chermaines-mbp.att.net>
2014-03-25 20:43:43 -0700

    fixes merge conflict

--- app/models/diabetic.rb
+++ app/models/diabetic.rb
@@ -22,9 +22,6 @@ class Diabetic < ActiveRecord::Base
 
   def get_data_for_graph(number_of_days=7, format = '%a %m/%d')
     collection = load_records(number_of_days)
-<<<<<<< HEAD
-    get_data(collection, format)
-=======
     get_data_2(collection)
   end
 
@@ -54,32 +51,10 @@ def get_data_2(collection)
 
   end
 
-  def format_days
-    data = self.sort_graph_data
-    puts data
->>>>>>> d65f96ee2fe072fe7f7972882dc4081ab063b00f
-  end
+  # def format_days
+  #   data = self.sort_graph_data
+  # end
 
-<<<<<<< HEAD
-  def get_data_for_pdf(number_of_days=14)
-    collection = load_records(number_of_days)
-    get_data(collection, '%m/%d/%y -- %I:%M %p')
-  end
-=======
-  def sort_graph_data
-    glucose_data = self.get_data_for_graph.first.sort_by{|a,b| a }
-    weight_data = self.get_data_for_graph.last.sort_by{|a,b| a }
-    glucose_data = Hash[*glucose_data.flatten]
-    weight_data = Hash[*weight_data.flatten]
-    data = [glucose_data, weight_data]
-  end
-
-  def format_days
-    data = self.sort_graph_data
-    puts data
-  end
-
->>>>>>> working x axis for graph
 
 
   private
