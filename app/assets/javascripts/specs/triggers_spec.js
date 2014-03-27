9e85a6b5db5225c1519abea39eb730172caf04f4
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-26 18:29:04 -0700

    Keep only good test. No time!

--- app/assets/javascripts/specs/triggers_spec.js
+++ app/assets/javascripts/specs/triggers_spec.js
@@ -1,27 +1,16 @@
 describe('Triggers', function(){
-  beforeEach(function(){
-    GlucoseAmigo.controller.redirectTo = function(data){
-      //Do nothing
-    }
-
-    // Mock Ajax
-    Ajax = (function(){
-      return {
-        logout: function(arg, callback){
-          // Ajax call and then fire the callback on success
-          callback();
-        }
-      }
-    }())
+  describe('login', function(){
+    it('should use redirectTo if you call login in the controller',function(){
+      spyOn(GlucoseAmigo.controller, 'redirectTo').and.callFake(function(){});
+      GlucoseAmigo.controller.login('test');
+      expect(GlucoseAmigo.controller.redirectTo).toHaveBeenCalledWith('test');
+    });
   });
-  it('should use redirectTo if you call login in the controller',function(){
-    spyOn(GlucoseAmigo.controller, 'redirectTo');
-    GlucoseAmigo.controller.login('test');
-    expect(GlucoseAmigo.controller.redirectTo).toHaveBeenCalledWith('test');
-  });
-  it("should call Ajax's logout and the callback when you logout", function(){
-    spyOn(GlucoseAmigo.controller, 'redirectTo');
-    GlucoseAmigo.controller.logout();
-    expect(GlucoseAmigo.controller.redirectTo).toHaveBeenCalled();
+  describe('logout', function(){
+    it("should call Ajax when you logout", function(){
+      spyOn(Ajax, 'logout').and.callFake(function(){});
+      GlucoseAmigo.controller.logout();
+      expect(Ajax.logout).toHaveBeenCalled();
+    });
   })
 });
